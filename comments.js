05f4ec08c2dcd6310e03fff0a2db6d70016acb43
CarlosJMB <86023100+CarlosJMB@users.noreply.github.com>
2024-05-28 21:16:59 +0000

    Third commit

--- comments.js
+++ comments.js
@@ -1,10 +1,93 @@
-//create a new web page
-function addComment() {
-    var comment = document.getElementById("comment").value;
-    var newComment = document.createElement("div");
-    newComment.innerText = comment;
-    document.getElementById("comments").appendChild(newComment);
-    document.getElementById("comment").value = "";
-}
+// Create web server
+// Create a web server and listen for incoming requests
 
-document.getElementById("add-comment").addEventListener("click", addComment);
+// Import express module
+const express = require('express');
+
+// Import mongoose module
+const mongoose = require('mongoose');
+
+// Import body-parser module
+const bodyParser = require('body-parser');
+
+// Import the Comment model
+const Comment = require('./models/comment');
+
+// Create a new express application
+const app = express();
+
+// Set the port number
+const port = 3000;
+
+// Connect to the database
+mongoose.connect('mongodb://localhost:27017/comments', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// Middleware to parse incoming request bodies
+app.use(bodyParser.json());
+
+// Define the root route
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+// Define a route to get all comments
+app.get('/comments', (req, res) => {
+    Comment.find({}, (err, comments) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.send(comments);
+        }
+    });
+});
+
+// Define a route to get a single comment
+app.get('/comments/:id', (req, res) => {
+    Comment.findById(req.params.id, (err, comment) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.send(comment);
+        }
+    });
+});
+
+// Define a route to create a new comment
+app.post('/comments', (req, res) => {
+    const comment = new Comment(req.body);
+
+    comment.save((err, savedComment) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.send(savedComment);
+        }
+    });
+});
+
+// Define a route to update a comment
+app.put('/comments/:id', (req, res) => {
+    Comment.findByIdAndUpdate(req.params.id, req.body, (err, comment) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.send(comment);
+        }
+    });
+});
+
+// Define a route to delete a comment
+app.delete('/comments/:id', (req, res) => {
+    Comment.findByIdAndDelete(req.params.id, (err, comment) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.send(comment);
+        }
+    });
+});
+
+// Start the web
